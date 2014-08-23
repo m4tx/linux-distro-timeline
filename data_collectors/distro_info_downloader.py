b46b98c203f54d55eef341d0c6367f6abdbfd4fd
Artur Jamro <artur.jamro@gmail.com>
2014-08-23 22:07:50 +0200

    Implemented downloading data from distrowatch
    Resolves #2

---
+++ data_collectors/distro_info_downloader.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python
+# python 2
+
+# This file is part of linux-distro-timeline.
+#
+# linux-distro-timeline is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# linux-distro-timeline is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with linux-distro-timeline; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+from pyquery import PyQuery
+from distro_ids import DISTRO_IDS
+from urllib import urlopen
+import json
+import os.path
+
+
+def get_html_object(url):
+    html_src = urlopen(url).read()
+    return PyQuery(html_src)
+
+
+def download_image(url, file_location):
+    if os.path.isfile(file_location) == True:
+        print('\t> Image already downloaded, skipping...')
+        return True
+    bin_data = urlopen(url).read()
+    result = False
+    with open(file_location, 'wb') as f:
+        result = f.write(bin_data) == None
+    if result == False and os.path.isfile(file_location):
+        os.remove(file_location)
+    return result
+
+
+def process_distro(distro_id, distro_dict, root):
+    print('Processing distro: %s' % distro_id)
+
+    url_base = 'http://distrowatch.com/table.php?distribution='
+    distro_url = url_base + distro_id
+    html = get_html_object(distro_url)
+    info_table = html.find('body')[0].xpath('//th[contains(text(),\'Release Date\')]/../..')[0]
+
+    cur_distro = distro_dict[distro_id]
+    cur_distro['id'] = distro_id
+    cur_distro['name'] = html.find('td.TablesTitle').find('h1').text()
+    cur_distro['release_date'] = info_table.xpath('//tr[2]//td[last()]')[0].text.replace('/', '-')
+    cur_distro['desktop_environment'] = info_table.xpath('//tr[8]//td[1]')[0].text
+    cur_distro['package_manager'] = info_table.xpath('//tr[9]//td[1]')[0].text
+    cur_distro['url'] = distro_url
+
+    parents = html.find('body')[0].xpath('//td[@class="TablesTitle"]/ul/li[2]/a/@href')
+    print('\t> DEBUG: parents: %s' % str(parents))
+    for p in parents:
+        if p == distro_id:
+            parents.remove(p)
+    if '' in parents:
+        parents.remove('')
+
+    if len(parents) == 0:
+        print('\t> Root distro!')
+    if len(parents) > 1:
+        print('\t> Multi parent! List: %s' % str(parents))
+
+    parent = distro_dict[parents[-1]] if parents else root
+    parent['children'].append(distro_dict[distro_id])
+
+    img_url = 'http://distrowatch.com/' + html.find('body')[0].xpath('//td[@class="TablesTitle"]/img/@src')[0]
+    extension = img_url.split('.')[-1]
+    file_location = '../data/img/%s.%s' % (distro_id, extension)
+    if not download_image(img_url, file_location):
+        print('\t> Image downloading error! Continuing...')
+
+
+distro_dict = dict()
+main_data = dict(id=0, children=[])
+# Init dict
+for distro_id in DISTRO_IDS:
+    distro_dict[distro_id] = dict(id=None,
+                                  name=None,
+                                  release_date=None,
+                                  desktop_environment=None,
+                                  package_manager=None,
+                                  url=None,
+                                  children=[])
+
+for distro_id in DISTRO_IDS:
+    process_distro(distro_id, distro_dict, main_data)
+
+data = json.dumps(distro_dict, sort_keys=True)
+success = False
+with open('../data/distro_info.json', 'w') as f:
+    success = f.write(data) == None
+print('Status: %s!', 'done' if success else 'fail')
